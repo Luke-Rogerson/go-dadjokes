b2280df58698417e890779081abdf727e87d783e
Luke Rogerson <lukerogerson@aol.com>
2020-08-02 21:33:36 +0100

    Get jokes from Reddit API

--- main.go
+++ main.go
@@ -3,8 +3,38 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"strings"
 )
+
+const userAgent = "lukedadjokesgo"
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatalln("Sorry, something went wrong: ", err)
+	}
+}
+
+func getJokes(query string) {
+	client := http.Client{}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://www.reddit.com/r/dadjokes/search.json?q=%s&restrict_sr=on&sort=top&t=all&count=10", query), nil)
+	req.Header.Set("User-Agent", userAgent)
+	handleError(err)
+
+	res, err := client.Do(req)
+	handleError(err)
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	handleError(err)
+
+	log.Println(string(body))
+}
 
 func getUserInput() string {
 	var input string
@@ -12,7 +42,7 @@ func getUserInput() string {
 		scanner := bufio.NewReader(os.Stdin)
 		input, _ = scanner.ReadString('\n')
 		if len(input) > 1 {
-			return input
+			return strings.TrimSuffix(input, "\n")
 		}
 		fmt.Println("You must enter something...")
 	}
@@ -21,5 +51,5 @@ func getUserInput() string {
 func main() {
 	fmt.Println("What do you want to hear a joke about?")
 	input := getUserInput()
-	fmt.Println("YAY", input)
+	getJokes(input)
 }
